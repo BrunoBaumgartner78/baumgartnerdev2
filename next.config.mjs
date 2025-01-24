1f676b29857304030ff562b631863c538ebb9810
BrunoBaumgartner78 <bruno@brainbloom.ch>
2025-01-24 22:00:50 +0100

    Deine Nachricht über die Änderungen

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,12 @@
-// next.config.mjs
-const nextConfig = {
+export default {
   reactStrictMode: true,
   async redirects() {
     return [
       {
-        source: '/(.*)',
-        has: [
-          {
-            type: 'host',
-            value: 'myiq.ch', // Ohne "www"
-          },
-        ],
-        destination: 'https://www.myiq.ch/:path*',
-        permanent: true, // 301 Redirect
-      },
-      {
-        source: '/(.*)',
-        has: [
-          {
-            type: 'host',
-            value: 'www.myiq.ch', // Mit "www"
-          },
-        ],
-        destination: 'https://www.myiq.ch/:path*',
-        permanent: true, // 301 Redirect
+        source: '/old-url',
+        destination: '/new-url',
+        permanent: true, // true bedeutet ein 301-Redirect
       },
     ];
   },
-};
-
-export default nextConfig;
+}
