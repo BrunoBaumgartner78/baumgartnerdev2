e4d6b36f56533f77e9ef377c5738da1286d01753
BrunoBaumgartner78 <bruno@brainbloom.ch>
2025-01-24 17:15:21 +0100

    Added redirect rules for canonical URLs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,32 @@
 // next.config.mjs
 const nextConfig = {
-    reactStrictMode: true,
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'host',
+            value: 'myiq.ch', // Ohne "www"
+          },
+        ],
+        destination: 'https://www.myiq.ch/:path*',
+        permanent: true, // 301 Redirect
+      },
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'host',
+            value: 'www.myiq.ch', // Mit "www"
+          },
+        ],
+        destination: 'https://www.myiq.ch/:path*',
+        permanent: true, // 301 Redirect
+      },
+    ];
+  },
+};
+
+export default nextConfig;
