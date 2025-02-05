d281a7a02afcd00f7b39a4e232be7bb22f9904c8
BrunoBaumgartner78 <bruno@brainbloom.ch>
2025-02-05 18:23:44 +0100

    Aktualisierte Konfiguration für Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,31 @@
+
 export default {
   async redirects() {
     return [
+ 
       {
-        source: "/:path*",
-        has: [{ type: "host", value: "myiq.ch" }],
-        destination: "https://www.myiq.ch/:path*",
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'myiq.ch',
+          },
+        ],
+        destination: 'https://www.myiq.ch/:path*',
         permanent: true,
       },
       {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.myiq.ch" }],
-        destination: "https://www.myiq.ch/:path*",
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.myiq.ch',
+          },
+        ],
+        destination: 'https://www.myiq.ch/:path*',
         permanent: true,
       },
     ];
   },
 };
+
