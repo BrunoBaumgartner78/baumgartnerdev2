1ba3fc8144ab3d597cb9f61648c026b278bfc874
BrunoBaumgartner78 <bruno@brainbloom.ch>
2025-02-05 18:30:24 +0100

    änderung next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,6 @@ const nextConfig = {
     return [
       {
         source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'myiq.ch',
-          },
-        ],
         destination: 'https://www.myiq.ch/:path*',
         permanent: true,
       },
